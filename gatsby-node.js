6f0aff9c3ccdb69c1318d0d33dfa1668452482a7
Toni Suominen <suomiton@gmail.com>
2019-01-24 12:57:06 +0200

    Fixed fetch issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const _ = require("lodash");
 const path = require("path");
 const axios = require("axios");
+const crypto = require("crypto");
 const { createFilePath } = require("gatsby-source-filesystem");
 const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 
@@ -75,45 +76,34 @@ exports.sourceNodes = async ({ actions }) => {
 
   const res = await fetchFormSubmissions();
 
-  // map into these results and create nodes
-  res.data.results.map((user, i) => {
+  res.data.map((submission, i) => {
     // Create your node object
-    const userNode = {
+    const submissionNode = {
       // Required fields
       id: `${i}`,
       parent: `__SOURCE__`,
       internal: {
-        type: `RandomUser` // name of the graphQL query --> allRandomUser {}
+        type: `Submission` // name of the graphQL query --> allRandomUser {}
         // contentDigest will be added just after
         // but it is required
       },
       children: [],
 
       // Other fields that you want to query with graphQl
-      gender: user.gender,
-      name: {
-        title: user.name.title,
-        first: user.name.first,
-        last: user.name.last
-      },
-      picture: {
-        large: user.picture.large,
-        medium: user.picture.medium,
-        thumbnail: user.picture.thumbnail
-      }
-      // etc...
+      name: submission.name,
+      body: submission.body
     };
 
     // Get content digest of node. (Required field)
     const contentDigest = crypto
       .createHash(`md5`)
-      .update(JSON.stringify(userNode))
+      .update(JSON.stringify(submissionNode))
       .digest(`hex`);
-    // add it to userNode
-    userNode.internal.contentDigest = contentDigest;
+
+    submissionNode.internal.contentDigest = contentDigest;
 
     // Create node with the gatsby createNode() API
-    // createNode(userNode);
+    createNode(submissionNode);
   });
 
   return;
--- src/templates/guestbook.js
+++ src/templates/guestbook.js
@@ -1,29 +1,66 @@
 import React from "react";
 import { graphql } from "gatsby";
+import styled from "styled-components";
 import Layout from "../components/Layout";
 import GuestbookForm from "../components/GuestbookForm";
 import Content, { HTMLContent } from "../components/Content";
 
-export const GuestbookTemplate = ({ title, content, contentComponent }) => {
+const GuestbookWrapper = styled.div`
+  margin: 0 auto;
+  padding: 30px;
+`;
+
+const GuestbookEntry = styled.div`
+  padding: 15px 0;
+`;
+
+const EntryName = styled.div`
+  line-height: 1.5;
+  font-size: 12px;
+`;
+
+const EntryBody = styled.div``;
+
+const renderEntry = (node, index) => (
+  <GuestbookEntry key={index}>
+    <EntryName>{node.name}</EntryName>
+    <EntryBody>{node.body}</EntryBody>
+  </GuestbookEntry>
+);
+
+export const GuestbookTemplate = ({
+  title,
+  entries,
+  content,
+  contentComponent
+}) => {
   const PageContent = contentComponent || Content;
   return (
     <>
       <h1>{title}</h1>
       <PageContent className="content" content={content} />
-      <GuestbookForm />
+      <GuestbookWrapper>
+        {entries.map((e, i) => renderEntry(e.node, i))}
+        <hr />
+        <GuestbookForm />
+      </GuestbookWrapper>
     </>
   );
 };
 
 const GuestbookPage = ({ data }) => {
-  const { markdownRemark: post } = data;
-
+  const {
+    markdownRemark: post,
+    allSubmission: { edges }
+  } = data;
+  console.log(edges);
   return (
     <Layout>
       <GuestbookTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        entries={edges}
       />
     </Layout>
   );
@@ -37,6 +74,14 @@ export const guestbookQuery = graphql`
       html
       frontmatter {
         title
+      }
+    }
+    allSubmission {
+      edges {
+        node {
+          name
+          body
+        }
       }
     }
   }
